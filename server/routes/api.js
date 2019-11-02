b7a174dd1d2ad03de310405667bb454aa4d38a1c
Chana <chana987@gmail.com>
2019-11-02 23:39:25 +0200

    total not quite working

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const moment = require('moment')
+const moment = require("moment")
 
-const Expense = require('../models/Expense')
+const Expense = require("../models/Expense")
 
 // const data = require('../models/expenses.json')
 
@@ -13,90 +13,90 @@ const Expense = require('../models/Expense')
 //     group: e.group
 // }).save())
 
-const find = function() {
-	Expense.find({ group: req.params.group })
-		.exec((err, group) => res.send(group))
-}
-
-router.get('/expenses', function(req, res) {
+router.get("/expenses", function(req, res) {
 	let group = req.query.group
 	let d1 = req.query.d1 || 0
-	let d2 = req.query.d2 || moment(new Date()).format('LLLL')
+	let d2 = req.query.d2 || moment(new Date()).format("LLLL")
 	let min = req.query.min || 0
 	let max = req.query.max || Infinity
 
 	if (group) {
 		Expense.find({
 			$and: [
-				{ date: { $gte: d1, $lte: d2 } }, 
+				{ date: { $gte: d1, $lte: d2 } },
 				{ amount: { $gte: min, $lte: max } },
 				{ group: group }
 			]
-		})
-		.exec( (err, data) => res.send(data))
+		}).exec((err, data) => res.send(data))
 	} else {
 		Expense.find({
 			$and: [
-				{ date: { $gte: d1, $lte: d2 } }, 
+				{ date: { $gte: d1, $lte: d2 } },
 				{ amount: { $gte: min, $lte: max } }
 			]
-		})
-		.exec( (err, data) => res.send(data))
+		}).exec((err, data) => res.send(data))
 	}
 })
 
-router.get('/total', function(req, res) {
+router.get("/total", function(req, res) {
 	let d1 = req.query.d1 || 0
-	let d2 = req.query.d2 || moment(new Date()).format('LLLL')
-	
+	let d2 = req.query.d2 || moment(new Date()).format("LLLL")
+
 	Expense.aggregate([
 		{
 			$match: { date: { $gte: d1, $lte: d2 } }
 		},
 		{
 			$group: {
-				_id: '$group',
+				_id: "$group",
 				total: {
-					$sum: '$amount'
+					$sum: "$amount"
 				}
 			}
-		}
-	]).exec((err, data) => res.send(data))
+		} 
+	], (err, data) => res.send(data))
 })
-	
-router.post('/new', function(req, res) {
+
+router.post("/new", function(req, res) {
 	let expense = new Expense({
 		name: req.body.name,
 		amount: req.body.amount,
 		date: req.body.date
-			? moment(req.body.date).format('LLLL')
-			: moment(new Date()).format('LLLL'),
+			? moment(req.body.date).format("LLLL")
+			: moment(new Date()).format("LLLL"),
 		group: req.body.group
 	})
 	expense
 		.save()
-		.then((err, expense) => res.send(`Spent ${expense.amount} on ${expense.name}`) )
+		.then((err, expense) =>
+			res.send(`Spent ${expense.amount} on ${expense.name}`)
+		)
 })
 
-router.put('/update/group', function(req, res) {
+router.put("/update/group", function(req, res) {
 	Expense.findOneAndUpdate(
 		{ name: req.body.name },
 		{ $set: { group: req.body.group } },
 		{ new: true }
-	).exec((err, expense) => res.send(`Changed group of ${expense.name} to ${expense.group}`) )
+	).exec((err, expense) =>
+		res.send(`Changed group of ${expense.name} to ${expense.group}`)
+	)
 })
 
-router.put('/update/amount', function(req, res) {
+router.put("/update/amount", function(req, res) {
 	Expense.findOneAndUpdate(
 		{ name: req.body.name },
 		{ $set: { amount: req.body.amount } },
 		{ new: true }
-	).exec((err, expense) => res.send(`Changed amount of ${expense.name} to ${expense.amount}`)	)
+	).exec((err, expense) =>
+		res.send(`Changed amount of ${expense.name} to ${expense.amount}`)
+	)
 })
 
-router.delete('/delete', function(req, res) {
-	Expense.findOneAndDelete({ name: req.body.name })
-	.exec((err, expense) => res.send(`Removed ${expense.name} from expenses`))
+router.delete("/delete", function(req, res) {
+	Expense.findOneAndDelete({ name: req.body.name }).exec((err, expense) =>
+		res.send(`Removed ${expense.name} from expenses`)
+	)
 })
 
 module.exports = router
